e1d939c4526f59993dc0beeb9a9f84b469f4625a
radosan <radoslaw.sanetra@gmail.com>
2023-08-19 18:26:45 +0200

    added making user profile

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -1,7 +1,8 @@
-class Randomness:
+class Predictor:
     def __init__(self, data=''):
         self.min_len = 100
         self.data = data
+        self.profile = {bin(key)[2:].zfill(3): [0, 0] for key in range(8)}
 
     @staticmethod
     def filter_data(raw_data):
@@ -14,12 +15,22 @@ class Randomness:
             if len(self.data) >= self.min_len:
                 break
             print(f'Current data length is {len(self.data)}, {self.min_len - len(self.data)} symbols left')
-        print(f'\nFinal data string:\n{self.data}')
+
+    def make_profile(self):
+        for i in range(0, len(self.data) - 3):
+            self.profile[self.data[i:i+3]][int(self.data[i+3])] += 1
+
+    def print_info(self):
+        print(f'\nFinal data string:\n{self.data}\n')
+        for triad, values in self.profile.items():
+            print(f'{triad}: {values[0]},{values[1]}')
 
 
 def main():
-    rand = Randomness()
-    rand.get_data()
+    predict = Predictor()
+    predict.get_data()
+    predict.make_profile()
+    predict.print_info()
 
 
 if __name__ == '__main__':
